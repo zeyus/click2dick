2e0b49e8c5c10c7f0fab6be67e0d22ee80f7e49b
zeyus <zeyus@zeyus.com>
2018-01-12 17:57:06 +1100

    Updated script to match instances of click where click is the start of the word.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,7 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-		document.body.innerHTML = document.body.innerHTML.replace(/click/ig, 'dick');
+		document.body.innerHTML = document.body.innerHTML.replace(/\bclick/ig, 'dick');
 	}
 	}, 10);
 });
